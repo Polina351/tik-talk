75bcac8393a6f748f93514ba6237599524e0cab2
Polina Deniskina <polinaoioi67@gmail.com>
2025-04-25 19:12:22 +0300

    feat: create component authorization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'authorization',
+        loadComponent: () => import('./pages/authorization/authorization.component').then(m => m.AuthorizationComponent),
+    }
+];
---
+++ src/app/pages/authorization/authorization.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authorization',
+  imports: [],
+  templateUrl: './authorization.component.html',
+  styleUrl: './authorization.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AuthorizationComponent {
+
+}
